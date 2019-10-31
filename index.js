04c2958451995f49f24020ccc24579a31eea3ed5
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-10-31 14:02:58 +0100

    added loader

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("hi");
+window.onload=loadingPage;
+
+function loadingPage() {
+    setTimeout(displayPage,3*1000);
+};
+
+function displayPage() {
+    document.querySelector("#spinner").classList.toggle("d-none");
+    document.querySelector(".main-page").classList.toggle("d-none");
+}
